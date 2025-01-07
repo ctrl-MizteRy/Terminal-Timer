92bf6e6955715ac8c4a5707eb04bfc0fefac6190
Khang <khang.ng.h.le@gmail.com>
2025-01-07 10:37:19 -0600

    Update terminal-timer.py

--- terminal-timer.py
+++ terminal-timer.py
@@ -1,30 +1,47 @@
 #!/usr/bin/env python3
 import argparse
-import subprocess
 from time import sleep
+from playsound import playsound
 import sys
+import schedule
+import os
 
 def main():
     args = argparse.ArgumentParser()
-    args.add_argument("--start", action="store_true", help="starting the timmer")
+    args.add_argument("--start", action="store_true", help="starting the timer")
     args.add_argument("-sec", type=check_value, default=0, help="The amount of seconds")
     args.add_argument("-min", type=check_value, default=0, help="The amount of minutes")
     args.add_argument("-hour", type=check_value, default=0, help="The amount of hours")
-
+    args.add_argument("-time", type=check_value, default=0, help="Select specific time the alarm go off in the format of 'hhmm' in 24hrs")
     argv = args.parse_args()
     if argv.start:
         try:
-            total = int(argv.sec) + (60* int(argv.min)) + (3600 * int(argv.hour))
-            if total == 0:
-                print("Cannot start a timer for 0 seconds")
-                sys.exit(0)
+            if argv.time > 0:
+                if argv.time < 1000:
+                    time_at = "0" + str(argv.time)[:2] + ":" + str(argv.time)[1:]
+                else:
+                    time_at = str(argv.time)[:2] + ":" + str(argv.time)[2:]
+                schedule.every().day.at(time_at).do(time_up)
+                print(f"Set alarm for {time_at}")
+                while True:
+                    schedule.run_pending()
+                    sleep(1)
+            
             else:
-                print(f"Starting a timer for {total} seconds")
-                for _ in range(1): sleep(total)
-                time_up()
+                total = int(argv.sec) + (60* int(argv.min)) + (3600 * int(argv.hour))
+                if total == 0:
+                    print("Cannot start a timer for 0 seconds")
+                    sys.exit(0)
+                else:
+                    print(f"Starting a timer for {total} seconds")
+                    for _ in range(1): sleep(total)
+                    time_up()
         except KeyboardInterrupt:
             print('\nTimer canceled')
             sys.exit(0)
+        except Exception as e:
+            print(f"Error occurd: {e}")
+            sys.exit(1)
     else:
         print("Missing Command-line Arguemnt")
         
